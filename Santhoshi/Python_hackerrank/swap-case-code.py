12b1dad169bb141a3c734d52f09657825d054c33
santhoshi-p <santhoshi.paladi@thundersoft.com>
2021-01-08 18:10:54 +0530

    Santhoshi_8_1_2021_Status

---
+++ Santhoshi/Python_hackerrank/swap-case-code.py
@@ -0,0 +1,22 @@
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+'''
+ouput
+
+Compiler Message
+Success
+Input (stdin)
+
+Download
+HackerRank.com presents "Pythonist 2".
+Expected Output
+
+Download
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+'''
